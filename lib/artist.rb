1ae3a79650453b214fcbf1f65ca7a4295ccb176a
peter.mbogo@student.moringaschool.com <peter.mbogo@student.moringaschool.com>
2022-10-25 01:01:31 +0300

    commit tree df88dfca4461a2168f72a5ddd0d93b14771e786f

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,10 +4,15 @@ class Artist
   attr_accessor :name
   attr_reader :songs
 
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
+  include Paramable
+
   @@artists = []
 
   def initialize
     @@artists << self
+    # super
     @songs = []
   end
 
@@ -19,13 +24,13 @@ class Artist
     @@artists
   end
 
-  def self.reset_all
-    self.all.clear
-  end
+  # def self.reset_all
+  #   self.all.clear
+  # end
 
-  def self.count
-    self.all.count
-  end
+  # def self.count
+  #   self.all.count
+  # end
 
   def add_song(song)
     @songs << song
@@ -36,7 +41,7 @@ class Artist
     songs.each { |song| add_song(song) }
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+  # def to_param
+  #   name.downcase.gsub(' ', '-')
+  # end
 end
